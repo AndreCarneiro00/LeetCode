1eb977330b879fe1b7a07ffb923c29896f8de4a4
AndreCarneiro00 <47666073+AndreCarneiro00@users.noreply.github.com>
2025-10-27 21:45:18 -0300

    Time: 7 ms (46.46%), Space: 12.5 MB (42.32%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        """
+        :type text: str
+        :rtype: int
+        balloon
+        """
+        char_counter = Counter(text)
+        return min([char_counter.get("b", 0), char_counter.get("a", 0), char_counter.get("l", 0)//2, char_counter.get("o", 0)//2, char_counter.get("n", 0)])
